8d6c0e0bcd0daec110b92d4d632d31893e475182
Jakub Hałun <jakubhal@gmail.com>
2018-07-01 20:50:05 +0200

    SpringBoot version updated

--- src/main/java/pl/halun/demo/bytebuddy/instrumented/app/InstrumentedApplication.java
+++ src/main/java/pl/halun/demo/bytebuddy/instrumented/app/InstrumentedApplication.java
@@ -1,9 +1,11 @@
 package pl.halun.demo.bytebuddy.instrumented.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class InstrumentedApplication {
 
 	public static void main(String[] args) {
